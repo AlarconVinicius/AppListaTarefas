e0ae32c6504a2eab013e27825142e63ae4ee661b
Vinícius Alarcon <alarcon.pqd74@gmail.com>
2022-01-24 13:08:54 -0300

    Função Recuperar

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # AppListaTarefas
 Projeto utilizando PHP com PDO e MySQL;
+
+# Criando Repositório
+git init
+git add README.md
+git status
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/AlarconVinicius/nome-repositorio.git
+git push -u origin main
+
+# Atualizando Repositório
+git add . 
+git status
+git commit -m "name commit"
+git push origin main
+
--- app_lista_tarefas_negoc/tarefa_controller.php
+++ app_lista_tarefas_negoc/tarefa_controller.php
@@ -6,20 +6,33 @@
     require "../app_lista_tarefas_negoc/tarefa.service.php";
     require "../app_lista_tarefas_negoc/conexao.php";
 
-    echo '<pre>';
+    /* echo '<pre>';
     print_r($_POST);
-    echo '</pre>';
+    echo '</pre>'; */
 
-    $tarefa = new Tarefa();
-    $tarefa->__set('tarefa', $_POST['tarefa']);
+    $acao = isset($_GET['acao']) ? $_GET['acao'] : $acao;
+    if( $acao == 'inserir') {
+        $tarefa = new Tarefa();
+        $tarefa->__set('tarefa', $_POST['tarefa']);
 
-    $conexao = new Conexao();
+        $conexao = new Conexao();
 
-    $tarefaService = new TarefaService($conexao, $tarefa);
-    $tarefaService->inserir();
+        $tarefaService = new TarefaService($conexao, $tarefa);
+        $tarefaService->inserir();
 
-    echo '<pre>';
+        header('Location: nova_tarefa.php?salvo=1');
+
+    } else if( $acao == 'recuperar' ) {
+        
+        $tarefa = new Tarefa();
+        $conexao = new Conexao();
+
+        $tarefaService = new TarefaService($conexao, $tarefa);
+        $tarefas = $tarefaService->recuperar();
+    }
+
+    /* echo '<pre>';
     print_r($tarefaService);
-    echo '</pre>';
+    echo '</pre>'; */
 
 ?>
